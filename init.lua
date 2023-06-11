6c21da54d5e566bb11cd86699782532258b05452
Luca388 <dkdls388@gmail.com>
2023-06-11 15:20:41 +0900

    modify

--- init.lua
+++ init.lua
@@ -5,17 +5,20 @@
 local inputEnglish = "com.apple.keylayout.ABC"
 local esc_bind
 --https://johngrib.github.io/wiki/hammerspoon-tutorial-03/
+-- ctrl+esc
 function convert_to_eng_with_esc()
 	local inputSource = hs.keycodes.currentSourceID()
 	if not (inputSource == inputEnglish) then
-		--hs.eventtap.keyStroke({}, 'right')
+		hs.eventtap.keyStroke({}, 'right')
 		hs.keycodes.currentSourceID(inputEnglish)
 	end
 	esc_bind:disable()
 	hs.eventtap.keyStroke({}, 'escape')
 	esc_bind:enable()
 end
+esc_bind = hs.hotkey.new({}, 'escape', convert_to_eng_with_esc):enable()
 --https://coldmater.tistory.com/177
+-- ctrl+[
 function hello()
 	local inputSource = hs.keycodes.currentSourceID()
 	if not (inputSource == inputEnglish) then
@@ -24,4 +27,5 @@ function hello()
 	hs.eventtap.keyStroke({}, 'escape')
 end
 hs.hotkey.bind({'control'}, 33, hello)
-esc_bind = hs.hotkey.new({}, 'escape', convert_to_eng_with_esc):enable()
+hs.hotkey.bind({'control'}, 'c', hello)
+hs.hotkey.bind({'control'}, 'o',hello)
